12764eebab5ebad055853aec46c78bb03deba8d0
Laia Castilla <lcastillaiamoros@gmail.com>
2023-10-25 11:32:58 +0200

    setModal on addTask is not working

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button, {SelectButton} from './Button'
 import TodoModal from './TodoModal';
 import style from "../styles/modules/app.module.scss"
 
 function AppHeader() {
+  
+  const [modalOpen,setModalOpen]=useState(false);
+
   return (
     <div className={style.appHeader}>
-      <Button variant="primary" >Add Task</Button>
+      <Button variant="primary"
+      onClick={() => setModalOpen(true)}
+
+      >
+        Add Task
+      </Button>
       <SelectButton id="status">
         <option value="all">All</option>
         <option value="incomplete">Incomplete</option>
         <option value="completed">Completed</option>
       </SelectButton>
-      <TodoModal/>
+      <TodoModal type="add" modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -3,36 +3,46 @@ import style from "../styles/modules/modal.module.scss"
 import {MdOutlineClose} from "react-icons/md"
 import Button from './Button';
 
-function TodoModal() {
+function TodoModal({modalOpen, setModalOpen}) {
   return (
-    <div className={style.wrapper}>
-      <div className={style.container}>
-        <div className={style.closeButton}>
-          <MdOutlineClose />
-        </div>
-        <form className={style.form}>
-          <h1 className={style.formTitle}>Add Task</h1>
-          <label htmlFor="title">
-            Title
-            <input type="text" id="title" />
-          </label>
-          <label htmlFor="status">
-            Status
-            <select name="status" id="status">
-              <option value="incomplete">Incomplete</option>
-              <option value="completed">Completed</option>
-            </select>
-          </label>
-          <div className={style.buttonContainer}>
-            <Button type="submit" variant="primary">
-              Add Task
-            </Button>
-            <Button type="button" variant="secondary">
-              Cancel
-            </Button>
+    <div>
+      {modalOpen && (
+        <div className={style.wrapper}>
+          <div className={style.container}>
+            <div
+              className={style.closeButton}
+              onClick={() => setModalOpen(false)}
+              onKeyDown={() => setModalOpen(false)}
+              tabIndex={0}
+              role="button"
+            >
+              <MdOutlineClose />
+            </div>
+            <form className={style.form}>
+              <h1 className={style.formTitle}>Add Task</h1>
+              <label htmlFor="title">
+                Title
+                <input type="text" id="title" />
+              </label>
+              <label htmlFor="status">
+                Status
+                <select name="status" id="status">
+                  <option value="incomplete">Incomplete</option>
+                  <option value="completed">Completed</option>
+                </select>
+              </label>
+              <div className={style.buttonContainer}>
+                <Button type="submit" variant="primary">
+                  Add Task
+                </Button>
+                <Button type="button" variant="secondary">
+                  Cancel
+                </Button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
