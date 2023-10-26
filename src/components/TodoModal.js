14ad96c17078b7cb3edd23a804624781a010376a
Laia Castilla <lcastillaiamoros@gmail.com>
2023-10-26 10:50:40 +0200

    add title and status state

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,56 +1,75 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from "../styles/modules/modal.module.scss"
 import {MdOutlineClose} from "react-icons/md"
 import Button from './Button';
 
 function TodoModal({type,modalOpen,setModalOpen}) {
+  const [title,setTitle]=useState("")
+  const [status, setStatus] = useState("pending")
+  function handleSubmit(event){
+    event.preventDefault()
+    console.log({title,status})
+  }
   return (
-    <div>
-      {modalOpen && (
-        <div className={style.wrapper}>
-          <div className={style.container}>
-            <div
-              className={style.closeButton}
-              onClick={() => setModalOpen(false)}
-              onKeyDown={() => setModalOpen(false)}
-              tabIndex={0}
-              role="button"
-            >
-              <MdOutlineClose />
-            </div>
-            <form className={style.form}>
-              <h1 className={style.formTitle}>Add Task</h1>
-              <label htmlFor="title">
-                Title
-                <input type="text" id="title" />
-              </label>
-              <label htmlFor="status">
-                Status
-                <select name="status" id="status">
-                  <option value="incomplete">Incomplete</option>
-                  <option value="completed">Completed</option>
-                </select>
-              </label>
-              <div className={style.buttonContainer}>
-                <Button type="submit" variant="primary">
-                  Add Task
-                </Button>
-                {/* I need to put this span with these options for it to work (???) */}
-                <span
-                  onClick={() => setModalOpen(false)}
-                  onKeyDown={() => setModalOpen(false)}
-                  tabIndex={0}
-                >
-                  <Button type="button" variant="secondary">
-                    Cancel
-                  </Button>
-                </span>
-              </div>
-            </form>
+    modalOpen && (
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div
+            className={style.closeButton}
+            onClick={() => setModalOpen(false)}
+            onKeyDown={() => setModalOpen(false)}
+            tabIndex={0}
+            role="button"
+          >
+            <MdOutlineClose />
           </div>
+          <form
+            className={style.form}
+            onSubmit={(event) => handleSubmit(event)}
+          >
+            <h1 className={style.formTitle}>Add Task</h1>
+            <label htmlFor="title">
+              Title
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </label>
+            <label htmlFor="status">
+              Status
+              <select
+                name="status"
+                id="status"
+                value={status}
+                onChange={(event) => setStatus(event.target.value)}
+              >
+                <option value="pending">Pending</option>
+                <option value="complete">Complete</option>
+                <option value="in progress">In progress</option>
+
+              </select>
+            </label>
+            <div className={style.buttonContainer}>
+              <Button type="submit" variant="primary">
+                Add Task
+              </Button>
+              {/* I need to put this span with these options for it to work (???) */}
+              <span
+                onClick={() => setModalOpen(false)}
+                onKeyDown={() => setModalOpen(false)}
+                tabIndex={0}
+              >
+                <Button type="button" variant="secondary">
+                  Cancel
+                </Button>
+              </span>
+            </div>
+          </form>
         </div>
-      )}
-    </div>
+      </div>
+    )
   );
 }
 
