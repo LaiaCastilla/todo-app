66ee53458296bd31680baebda9c3963f7f1f12df
Laia Castilla <lcastillaiamoros@gmail.com>
2023-10-27 13:45:58 +0200

    set up todo list

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 
 function AppContent() {
+  const todoList = useSelector((state) => state.todo.todoList);
+  // copy of todoList ...todoList
+  const sortedTodoList= [...todoList];
+  // sort to do list
+  sortedTodoList.sort((a,b)=>new Date(b.time)-new Date(a.time));
   return (
-    <div>This is App content</div>
+    <div>
+      {sortedTodoList && sortedTodoList.length > 0 ?
+        sortedTodoList.map((todo)=>todo.title)
+      : "no todo found"}
+    </div>
   )
 }
 
