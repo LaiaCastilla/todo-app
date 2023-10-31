35a323b40e546bb22a707c5ed1fdf1440b8a8f6d
Laia Castilla <lcastillaiamoros@gmail.com>
2023-10-31 09:32:25 +0100

    add edit button to list and connect to functions

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import style from "../styles/modules/todoItem.module.scss"
 import { getClasses } from '../utils/getClasses'
-import { MdDelete} from "react-icons/md";
+import { MdDelete, MdEdit} from "react-icons/md";
 
 
 
 
 function TodoItem({todo}) {
+  const handleDelete=()=>{
+    console.log("Deleting")
+  }
+    const handleEdit = () => {
+      console.log("Editing");
+    };
   return (
     <div className={style.item}>
       <div className={style.todoDetails}>
@@ -22,14 +28,27 @@ function TodoItem({todo}) {
           >
             {todo.title}
           </p>
-          <p className={style.time}>{todo.time}
-          </p>
+          <p className={style.time}>{todo.time}</p>
         </div>
       </div>
       <div className={style.todoActions}>
-        <div className={style.icon}>
-          <MdDelete/>
-        
+        <div
+          className={style.icon}
+          onClick={handleDelete}
+          onKeyDown={handleDelete}
+          role="button"
+          tabIndex={0}
+        >
+          <MdDelete />
+        </div>
+        <div
+          className={style.icon}
+          onClick={handleEdit}
+          onKeyDown={handleEdit}
+          role="button"
+          tabIndex={0}
+        >
+          <MdEdit />
         </div>
       </div>
     </div>
