d5c512e4995d4f059a73936c6f2d4bfa09a55aaa
Laia Castilla <lcastillaiamoros@gmail.com>
2023-11-01 11:29:31 +0100

    filter done

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -10,10 +10,19 @@ function AppContent() {
   const sortedTodoList= [...todoList];
   // sort to do list
   sortedTodoList.sort((a,b)=>new Date(b.time)-new Date(a.time));
+  // filter
+  const filterStatus=useSelector((state)=>state.todo.filterStatus);
+  const filteredTodoList=sortedTodoList.filter((item) => {if(filterStatus==="all")
+  {return true;
+  }
+return item.status === filterStatus;
+});
+   
+  
   return (
     <div className={style.content__wrapper}>
-      {sortedTodoList && sortedTodoList.length > 0 ?
-        sortedTodoList.map((todo)=><TodoItem key={todo.id} todo={todo}/>)
+      {filteredTodoList && filteredTodoList.length > 0 ?
+        filteredTodoList.map((todo)=><TodoItem key={todo.id} todo={todo}/>)
       : "no todo found"}
     </div>
   )
